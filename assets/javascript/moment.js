d11bd87596e6482e7b09c74144e6051d007cb38c
brandongatlin <brandongatlin1981@me.com>
2017-10-28 09:38:21 -0500

    changed theme to futurama

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -1,11 +1,11 @@
-
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyDtXSmCAdx6puEAfvwr7I5kxv_1JEBj6_s",
     authDomain: "trainschedule-d611e.firebaseapp.com",
     databaseURL: "https://trainschedule-d611e.firebaseio.com",
     projectId: "trainschedule-d611e",
     storageBucket: "",
     messagingSenderId: "1026338878177"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
+
