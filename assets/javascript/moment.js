78b4592233a5e077e9d55ddce43da9e8715133b5
brandongatlin <brandongatlin1981@me.com>
2017-10-28 17:22:17 -0500

    made variables and such

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -7,5 +7,34 @@ var config = {
     storageBucket: "",
     messagingSenderId: "1026338878177"
 };
+
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
+var craftName = ""
+var craftDestination = ""
+var craftFrequency
+var nextDeparture
+var minutesAway
+
+$("#submit").on("click", function() {
+            event.preventDefault();
+
+            craftName = $("#craftName").val().trim();
+            craftDestination = $("#destination").val().trim();
+            nextDeparture = $("#departure").val().trim();
+            craftFrequency = $("#frequency").val().trim();
+
+            $("#craftName").val("");
+            $("#destination").val("");
+            $("#departure").val("");
+            $("#frequency").val("");
+
+            database.ref().on("child_added", function(snapshot) {
+                var craftName = snapshot.val().craftName;
+console.log(snapshot)
+
+});
+
+            })
