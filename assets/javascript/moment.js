be045660f858d962340b36778c0aa6cdee7ed92f
brandongatlin <brandongatlin1981@me.com>
2017-10-30 21:11:51 -0500

    tried adding minutes remaining

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -24,7 +24,7 @@ $("#submit").on("click", function() {
 
     craftName = $("#craftNameBox").val().trim();
     craftDestination = $("#destinationBox").val().trim();
-    nextDeparture = $("#departureBox").val().trim();
+    firstDeparture = $("#firstDepartureBox").val().trim();
     craftFrequency = $("#frequencyBox").val().trim();
     minutesAway = ""
 
@@ -32,7 +32,7 @@ $("#submit").on("click", function() {
         craftName: craftName,
         craftDestination: craftDestination,
         craftFrequency: craftFrequency,
-        nextDeparture: nextDeparture,
+        firstDeparture: firstDeparture,
         minutesAway: minutesAway
 
     })
@@ -46,11 +46,12 @@ database.ref().on("child_added", function(snapshot) {
     var craftName = (snapshot.val().craftName);
     var craftDestination = (snapshot.val().craftDestination);
     var craftFrequency = (snapshot.val().craftFrequency);
-    var nextDeparture = (snapshot.val().nextDeparture);
+    var firstDeparture = (snapshot.val().firstDeparture);
     console.log(snapshot.val());
 
-    // why the hell is this printing twice?!!!! //
-    $("#table").append("<tr><td>" + craftName + "</td><td>" + craftDestination + "</td><td>" + nextDeparture + "</td><td>" + craftFrequency + "</td></tr>");
+    $("#table").append("<tr><td>" + craftName + "</td><td>" +
+        craftDestination + "</td><td>" + firstDeparture + "</td><td>" +
+        craftFrequency + "</td><td>" + minutesAway + "</td></tr>");
 
     $("#craftNameBox").val("");
     $("#destinationBox").val("");
@@ -60,3 +61,61 @@ database.ref().on("child_added", function(snapshot) {
     console.log(craftName)
 
 });
+
+// time code
+//Creates a string of current date YYYY-MM-DD
+		var stringTime = moment().format("YYYY-MM-DD");
+
+		//if we have some in 24 hour time as string
+		var firstTrain = (firstDeparture);
+
+		//creats a moment.js object with current date and time of firstTrain 
+		//can also use " ". ex 2013-02-08 24:00 or 2013-02-08T24:00
+		var test2 = moment(stringTime + "T" + firstTrain);  
+		console.log(test2)
+
+		// displays  12 hour time (hh:mm) and AM or PM
+		console.log("test2: " + test2.format("HH:MM"));
+
+		//displays  Month Day 12 hour time (hh:mm) and AM or PM
+		console.log(test2.format("MM DD HH:MM"));	
+
+		// Gets us current date and time;
+		var now = moment();
+		console.log("now:", now.format("HH:MM")); 
+
+		
+		// set date/ by string-format
+		var day = moment("1995-12-25", "YYYY-MM-DD"); 
+		console.log("day:", day.format("MM DD YYYY"));
+
+
+		//Get DIFFERENCT BETWEEN TIMES		
+		var diff = now.diff(test2, "minutes");
+		console.log("diff in minutes:", diff);
+
+//Calculates and returns time of next train arrival.
+function getNextArrival(firstDeparture, frequency) {
+    //Initilizes to first train time.
+    var nextArrival = moment(firstDeparture);
+
+    //While nextArrival is less than current time, add train frequency to nextArrival;
+    while (nextArrival < moment()) {
+        nextArrival.add(frequency, "minutes");
+    };
+
+    return nextArrival;
+
+} //END getNextArrival
+
+function getMinutesAway(time) {
+    //Returns the difference in minutes bewteen trains next arrival and currrnt time.
+    //.diff() always rounds toward zero (down if pos.) so 1.59 would be 1 minute.
+    //adding 'true' argument returns floating point.
+    //Which I round using Math.round so 1.59 would round up to 2.
+
+    var minutesAway = Math.round(getNextArrival(time).diff(moment(), "minutes", true));
+
+    return minutesAway
+
+} //END getMinutesAway
